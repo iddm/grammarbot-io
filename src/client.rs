78937d82328c0a1bb2edee8546710229a1910a47
Victor Polevoy <fx@thefx.co>
2020-03-01 11:27:12 +0100

    Update the dependencies.
    Signed-off-by: Victor Polevoy <fx@thefx.co>

--- src/client.rs
+++ src/client.rs
@@ -9,7 +9,7 @@ impl Request {
     /// Sends the request using the provided client.
     pub fn send_with_client(
         &self,
-        client: &reqwest::Client,
+        client: &reqwest::blocking::Client,
     ) -> Result<crate::response::Response, reqwest::Error> {
         HttpRequest::from(self).send_with_client(client)
     }
@@ -18,13 +18,13 @@ impl Request {
 impl HttpRequest {
     /// Sends the request and returns the response using default client.
     pub fn send(&self) -> Result<crate::response::Response, reqwest::Error> {
-        self.send_with_client(&reqwest::Client::new())
+        self.send_with_client(&reqwest::blocking::Client::new())
     }
 
     /// Sends the request using the provided client.
     pub fn send_with_client(
         &self,
-        client: &reqwest::Client,
+        client: &reqwest::blocking::Client,
     ) -> Result<crate::response::Response, reqwest::Error> {
         let request = match self.method {
             HttpRequestMethod::Get => client.get(&self.url).query(&self.values),
@@ -38,8 +38,15 @@ impl HttpRequest {
 mod tests {
     #[test]
     fn test_request_without_api_key() {
-        assert!(super::Request::from("Hello this grammarly world!")
-            .send()
-            .is_ok());
+        assert!(
+            super::Request::from("Hello this grammarly world!")
+                .send()
+                .unwrap()
+                .software
+                .version
+                .0
+                .len()
+                > 0
+        );
     }
 }
